ed5b3292b9e4171d42b3a50ea177d2ff0371b92c
Minh Anh Nguyen <minhanhn@usf.edu>
2025-09-23 16:37:14 -0400

    first commit

---
+++ cc_05.js
@@ -0,0 +1,8 @@
+employee = [
+    {name: "Jim", hourlyRate: 40, hoursWorked: 30},
+    {name: "Jane", hourlyRate: 60, hoursWorked: 40},
+    {name: "Jay", hourlyRate: 100, hoursWorked: 20},
+    {name: "Jone", hourlyRate: 30, hoursWorked: 40},
+    {name: "June", hourlyRate: 50, hoursWorked: 60}
+];
+    
